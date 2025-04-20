8bb2e4a3bbb9c18655f1c0b99205edc6bc4167bd
Fevzi Can Sahinler <fevzisahinler@gmail.com>
2025-04-20 19:52:19 +0300

    Initial Commit

---
+++ YeniParaMobile/YeniParaMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeniParaMobileApp.swift
+//  YeniParaMobile
+//
+//  Created by Fevzi Sahinler on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct YeniParaMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
